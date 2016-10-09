761f1e571bb4dd0d0aa9a541befa5a29809d93dd
Tunde Michael <tm@tundemichael.com>
2016-10-09 22:01:14 +0100

    Another update

--- src/com/tundemichael/blog/jsonlib/JsonLib.java
+++ src/com/tundemichael/blog/jsonlib/JsonLib.java
@@ -15,14 +15,13 @@ public class JsonLib {
     private static final Logger LOG = Logger.getLogger(JsonLib.class.getName());
 
     public static void main(String[] args) {
-
         try {
             System.out.println("================== Ant ====================");
-            System.out.println("Concated String --> " + concatString());
+            System.out.println("Concated String --> " + buildWithConcatedString());
             System.out.println("Built with Gson --> " + buildWithGson());
             System.out.println("Built with Jackson --> " + buildWithJackson());
             System.out.println("Built with Primefaces --> " + buildWithPrimefaces());
-            System.out.println("Built with Java EE 7  --> " + buildWithJavaEE7());
+            System.out.println("Built with Java EE 7  --> " + buildWithJSONP());
             System.out.println("Built with Jenson --> " + buildWithGson());
         } catch (Exception e) {
             LOG.log(Level.SEVERE, null, e);
@@ -30,50 +29,50 @@ public class JsonLib {
 
     }
 
-    public static String concatString() {
+    public static String buildWithConcatedString() {
         String json = "{"
-                + "\"firstname\":\"Tunde\","
-                + "\"lastname\":\"Michael\","
+                + "\"firstname\":\"John\","
+                + "\"lastname\":\"Doe\","
                 + "\"phone\":\"2348xxxxx3477\","
-                + "\"email\":\"tm@tundemichael.com\""
+                + "\"email\":\"john@johndoe.com\""
                 + "}";
         return json;
     }
 
     public static String buildWithGson() {
         JsonObject json = new JsonObject();
-        json.addProperty("firstname", "Tunde");
-        json.addProperty("lastname", "Michael");
+        json.addProperty("firstname", "John");
+        json.addProperty("lastname", "Doe");
         json.addProperty("phone", "2348xxxxx3477");
-        json.addProperty("email", "tm@tundemichael.com");
+        json.addProperty("email", "john@johndoe.com");
         return json.toString();
     }
 
     public static String buildWithJackson() {
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode json = mapper.createObjectNode();
-        json.put("firstname", "Tunde");
-        json.put("lastname", "Michael");
+        json.put("firstname", "John");
+        json.put("lastname", "Doe");
         json.put("phone", "2348xxxxx3477");
-        json.put("email", "tm@tundemichael.com");
+        json.put("email", "john@johndoe.com");
         return json.toString();
     }
 
     public static String buildWithPrimefaces() throws JSONException {
         JSONObject json = new JSONObject();
-        json.put("firstname", "Tunde");
-        json.put("lastname", "Michael");
+        json.put("firstname", "John");
+        json.put("lastname", "Doe");
         json.put("phone", "2348xxxxx3477");
-        json.put("email", "tm@tundemichael.com");
+        json.put("email", "john@johndoe.com");
         return json.toString();
     }
 
-    public static String buildWithJavaEE7() {
+    public static String buildWithJSONP() {
         JsonObjectBuilder json = Json.createObjectBuilder();
-        json.add("firstname", "Tunde");
-        json.add("lastname", "Michael");
+        json.add("firstname", "John");
+        json.add("lastname", "Doe");
         json.add("phone", "2348xxxxx3477");
-        json.add("email", "tm@tundemichael.com");
+        json.add("email", "john@johndoe.com");
         return json.build().toString();
     }
 
